efebfe2dbd9f9da69434f3a12fa8fd8536b971b3
Gilvan Andre Gobbato <gilvan.gobbato@cwi.com.br>
2020-08-04 22:02:24 -0300

    Finalizando projeot

--- secao_3/src/main/java/com/github/gilvangobbato/api/controller/ClienteController.java
+++ secao_3/src/main/java/com/github/gilvangobbato/api/controller/ClienteController.java
@@ -2,6 +2,7 @@ package com.github.gilvangobbato.api.controller;
 
 import com.github.gilvangobbato.domain.entity.Cliente;
 import com.github.gilvangobbato.domain.repository.ClientesRepository;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
@@ -11,6 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
+@Api("Api Clientes")
 @RestController
 @RequestMapping("/api/clientes")
 public class ClienteController {
@@ -18,8 +20,13 @@ public class ClienteController {
     @Autowired
     private ClientesRepository repository;
 
+    @ApiOperation("Obter detalhes de um cliente")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cliente encontrado"),
+            @ApiResponse(code = 400, message = "Cliente não encontrado"),
+    })
     @GetMapping("/{id}")
-    public Cliente getClienteById(@PathVariable("id") Long id) {
+    public Cliente getClienteById(@PathVariable("id") @ApiParam("Id do cliente") Long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Cliente não encontrado"));
@@ -31,7 +38,11 @@ public class ClienteController {
     public List<Cliente> getAll() {
         return repository.findAll();
     }
-
+    @ApiOperation("Salva um cliente")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Cliente salvo com sucesso"),
+            @ApiResponse(code = 400, message = "Erros de validação"),
+    })
     @PostMapping(value = "")
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente save(@RequestBody Cliente entity) {
